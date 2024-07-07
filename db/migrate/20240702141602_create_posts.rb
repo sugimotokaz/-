85f459fefc74220e3a50410a9afca76635c1c669
@sugimotokaz <aaddggjjmmpppttww@gmail.com>
2024-07-08 00:35:35 +0900

    フラッシュメッセージ修正

--- db/migrate/20240702141602_create_posts.rb
+++ db/migrate/20240702141602_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.string :body
+      t.text :body
       t.string :video, null: false
 
       t.timestamps
