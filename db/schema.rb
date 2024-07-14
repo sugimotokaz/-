4e829f53d6bea1761e70b432dacbc2848b07a22b
@sugimotokaz <aaddggjjmmpppttww@gmail.com>
2024-07-14 22:07:33 +0900

    掲示板がない場合の設定

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,8 @@
 ActiveRecord::Schema[7.1].define(version: 2024_07_04_063402) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
-    t.text "body"
-    t.string "video", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -31,5 +27,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_04_063402) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
